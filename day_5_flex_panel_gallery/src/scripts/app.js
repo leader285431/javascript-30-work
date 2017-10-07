20ec3ffbce0bc13badba7ec52180a7f202d3b382
leader285431 <leader285431@outlook.com>
2017-10-07 11:14:36 +0700

    day 5

---
+++ day_5_flex_panel_gallery/src/scripts/app.js
@@ -0,0 +1,15 @@
+var panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    if(e.propertyName.includes('flex'))
+        this.classList.toggle('open-active');
+}
+
+panels.forEach(function(panel) {
+    panel.addEventListener('click', toggleOpen);
+    panel.addEventListener('transitionend', toggleActive);
+})
